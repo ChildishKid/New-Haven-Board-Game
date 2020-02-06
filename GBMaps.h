9713f85346147d585964c9840e46a3a0b1a09e3b
Jamal <jamal.g16@hotmail.com>
2020-02-06 01:14:03 -0500

    completed part 6i and modified gameboard accordingly

--- GBMaps.h
+++ GBMaps.h
@@ -7,53 +7,73 @@ using namespace std;
 
 class GBMaps {
 
-public:
-	class Square {
 	public:
-		Square(int x_value, int y_value);
-		~Square();
+		class Node {
+			public:
+				Node(Type* type);
+				Type getType();
+				void setType(Type* type);
 
-		void setTopLeft(Type tL);
-		void setTopRight(Type tR);
-		void setBottomLeft(Type bL);
-		void setBottomRight(Type bR);
-		void setX(int x_value);
-		void setY(int y_value);
-		void setStatus(bool value);
-		void addAdj(Square* obj);
+			private:
+				Type* type;
+				friend bool operator==(const GBMaps::Node &lhs, const GBMaps::Node &rhs);
+				friend bool operator!=(const GBMaps::Node &lhs, const GBMaps::Node &rhs);
+		};
 
-		Type getTopLeft() const;
-		Type getTopRight() const;
-		Type getBottomLeft() const;
-		Type getBottomRight() const;
-		vector<Square*> getAdj() const;
-		int getX() const;
-		int getY() const;
-		int getSize() const;
+		class Square {
+			public:
+				Square(int x_value, int y_value);
+				~Square();
 
+				void setTopLeft(Node tL);
+				void setTopRight(Node tR);
+				void setBottomLeft(Node bL);
+				void setBottomRight(Node bR);
+				void setX(int x_value);
+				void setY(int y_value);
+				void setStatus(bool value);
+				void addAdj(Square* obj);
+
+				Node* getTopLeft();
+				Node* getTopRight();
+				Node* getBottomLeft();
+				Node* getBottomRight();
+				vector<Square*> getAdj() const;
+				int getX() const;
+				int getY() const;
+				int getSize() const;
+
+
+			private:
+				Node* topLeft;
+				Node* topRight;
+				Node* bottomLeft;
+				Node* bottomRight;
+				int* x;
+				int* y;
+				bool* status;
+				vector<Square*>* adjacent;
+				int* size;
+		};
+
+		map<pair<int, int>, Square*>::iterator iterate;
+
+		GBMaps(int numOfPlayers);
+		Square* getSquare(int x_value, int y_value);
+		map<pair<int, int>, Square*>::iterator begin();
+		map<pair<int, int>, Square*>::iterator end();
+		int getHeight();
+		int getWidth();
+		int getNumberOfPlayers();
+		bool squareAboveExists(int x_value, int y_value);
+		bool squareBelowExists(int x_value, int y_value);
+		bool squareToLeftExists(int x_value, int y_value);
+		bool squareToRightExists(int x_value, int y_value);
 
 	private:
-		Type* topLeft;
-		Type* topRight;
-		Type* bottomLeft;
-		Type* bottomRight;
-		int* x;
-		int* y;
-		bool* status;
-		vector<Square*>* adjacent;
-		int* size;
-
-	};
-
-	map<pair<int, int>, Square*>::iterator iterate;
-
-	GBMaps(int numberOfPlayers);
-	Square* getSquare(int x_value, int y_value) const;
-	map<pair<int, int>, Square*>::iterator begin();
-	map<pair<int, int>, Square*>::iterator end();
-
-private:
-	map<pair<int, int>, Square*>* gameBoard;
-	void setUpBoard(int numberOfPlayers);
-
+		map<pair<int, int>, Square*>* gameBoard;
+		int* height;
+		int* width;
+		int* numberOfPlayers;
+		void setUpBoard();
 };
