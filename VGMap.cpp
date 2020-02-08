06afa78f011c3abc680b8a65a8782c02845a9736
ThomasGillard <38733378+ThomasGillard@users.noreply.github.com>
2020-02-08 13:49:04 -0500

    Reimplementation of part 2 to be similar to part 1

--- VGMap.cpp
+++ VGMap.cpp
@@ -1,61 +1,145 @@
 #include <iostream>
 #include <cstdlib>
+#include<map>
+#include<vector>
 #include "VGMap.h"
 
 using namespace std;
 
+VGMap::Circle::Circle(int x_value, int y_value) {
+	x = new int(x_value);
+	y = new int(y_value);
+	colour = NULL;
+	cost = new int(y_value + 1);
+	status = NULL;
+	adjacent = new vector<Circle*>();
+}
 
-Graph* createGraph(int X, int Y) {
-	Graph *graph = new Graph;
-	graph->x = X;
-	graph->y = Y;
-	graph->arr = new AdjList*[X];
-	for (int i = 0; i < X; i++) {
-		graph->arr[i] = new AdjList[Y];
-	}
+VGMap::Circle::Circle() {
+	x = 0;
+	y = 0;
+	colour = NULL;
+	cost = 0;
+	status = NULL;
+	adjacent = new vector<Circle*>();
+}
 
-	for (int i = 0; i < X; i++) {
-		for (int j = 0; j < Y; j++) {
-			graph->arr[i][j].head = NULL;
+VGMap::Circle::~Circle() {
+	delete x;
+	delete y;
+	delete colour;
+	delete cost;
+	delete status;
+	delete adjacent;
+	x = NULL;
+	y = NULL;
+	colour = NULL;
+	cost = NULL;
+	status = NULL;
+	adjacent = NULL;
+}
+
+void VGMap::Circle::setX(int x_value) {
+	*x = x_value;
+}
+
+void VGMap::Circle::setY(int y_value) {
+	*y = y_value;
+}
+
+void VGMap::Circle::setColour(Type clr) {
+	*colour = clr;
+}
+
+void VGMap::Circle::setCost(int val) {
+	*cost = val;
+}
+
+void VGMap::Circle::setStatus(string stat) {
+	*status = stat;
+}
+
+void VGMap::Circle::addAdj(Circle* obj) {
+	adjacent->push_back(obj);
+}
+
+
+int VGMap::Circle::getX() const {
+	return *x;
+}
+
+int VGMap::Circle::getY() const {
+	return *y;
+}
+
+Type VGMap::Circle::getColour() const {
+	return *colour;
+}
+
+int VGMap::Circle::getCost() const{
+	return *cost;
+}
+
+string VGMap::Circle::getStatus() const {
+	return *status;
+}
+
+vector<VGMap::Circle*> VGMap::Circle::getAdj() const {
+	return *adjacent;
+}
+
+VGMap::VGMap(string player) {
+	playerName = player;
+	playerBoard = new map<pair<int, int>, Circle*>();
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 6; j++) {
+			(*playerBoard)[{i, j}] = new VGMap::Circle(i, j);
 		}
 	}
 
-	return graph;
-}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 6; j++) {
 
-
-Node *newNode(Coords *vals) {
-	Node *ptr = new Node;
-	ptr->data = vals;
-	ptr->next = NULL;
-	return ptr;
-}
-
-void addEdge(Graph *graph, Coords *src, Coords *dest) {
-	//add node at beginning of arr
-	Node *ptr = newNode(dest);
-	ptr->next = graph->arr[src->x][src->y].head;
-	graph->arr[src->x][src->y].head = ptr;
-	//connect from dest to src
-	ptr = newNode(src);
-	ptr->next = graph->arr[dest->x][dest->y].head;
-	graph->arr[dest->x][dest->y].head = ptr;
-}
-
-void printGraph(Graph* graph) {
-	//loop over every adjacency list
-	for (int i = 0; i < graph->x; i++) {
-		for (int j = 0; j < graph->y; j++) {
-			Node *root = graph->arr[i][j].head;
-			cout << "Adjacency list of vertex " << i + 1 << "," << j + 1 << endl;
-			//loop over each node in list
-			while (root != NULL) {
-				cout << root->data->x + 1 << "," << root->data->y + 1 << " -> ";
-				root = root->next;
+			if (!(i - 1 < 0)) {
+				(*playerBoard)[{i, j}]->addAdj((*playerBoard)[{(i - 1), j}]);
 			}
-			cout << endl;
+
+			if (!(i + 1 > 4)) {
+				(*playerBoard)[{i, j}]->addAdj((*playerBoard)[{(i + 1), j}]);
+			}
+
+			if (!(j - 1 < 0)) {
+				(*playerBoard)[{i, j}]->addAdj((*playerBoard)[{i, (j - 1)}]);
+			}
+
+			if (!(j + 1 > 5)) {
+				(*playerBoard)[{i, j}]->addAdj((*playerBoard)[{i, (j + 1)}]);
+			}
+
 		}
 	}
+
+
 }
 
+VGMap::VGMap() {
+	playerName = "";
+	playerBoard = new map<pair<int, int>, Circle*>();
+}
 
+VGMap::~VGMap() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 6; j++) {
+			delete (*playerBoard)[{i, j}];
+			(*playerBoard)[{i, j}] = NULL;
+		}
+	}
+
+	delete playerBoard;
+	playerBoard = NULL;
+}
+
+VGMap::Circle* VGMap::getCircle(int x_value, int y_value) const {
+	return (*playerBoard).at({ x_value, y_value });
+}
--- VGMap.h
+++ VGMap.h
@@ -2,33 +2,50 @@
 
 #include<iostream>
 #include<cstdlib>
+#include<vector>
+#include<map>
+#include "Resources.h"
 
 using namespace std;
 
-struct Coords {
-	int x;
-	int y;
+class VGMap {
+public:
+	class Circle {
+	public:Circle(int x_value, int y_value);
+		   Circle();
+		~Circle();
+
+		void setX(int x);
+		void setY(int y);
+		void setColour(Type colour);
+		void setCost(int val);
+		void setStatus(string stat);
+		void addAdj(Circle* obj);
+
+		int getX() const;
+		int getY() const;
+		Type getColour() const;
+		int getCost() const;
+		string getStatus() const;
+		vector<Circle*> getAdj() const;
+
+	private: 
+		int* x;
+		int* y;
+		Type* colour;
+		int* cost;
+		string* status;
+		vector<Circle*>* adjacent;
+	};
+
+	VGMap(string player);
+	VGMap();
+	~VGMap();
+	Circle* getCircle(int x_value, int y_value) const;
+	
+
+private:
+	map<pair<int, int>, Circle*>* playerBoard;
+	string playerName;
 };
-
-struct Node{
-	Coords *data;
-	Node *next;
-};
-
-struct AdjList{
-	Node *head;
-};
-
-struct Graph{
-	int x;
-	int y;
-	AdjList **arr;
-};
-
-
-
-Node *newNode(int);
-Graph *createGraph(int, int);
-void addEdge(Graph*, Coords*, Coords*);
-void printGraph(Graph*);
 
