fe58adcd34e3fd56785c9497cd64358c0ee1978c
Jamal <jamal.g16@hotmail.com>
2020-02-09 16:02:08 -0500

    removed unnecessary test

--- ResourcesDriver.cpp
+++ ResourcesDriver.cpp
@@ -7,12 +7,4 @@ int main() {
     GBMaps* gbMap = new GBMaps(2);
     ResourceCounter* resourceCounter = new ResourceCounter(gbMap);
     Hand* hand = new Hand(deck, resourceCounter);
-
-    cout << "========== TEST ==========" << endl;
-    cout << "Enter the following coordinates: (1, 3)" << endl;
-    cout << "Expected sheep resources gained at (1, 3): 1" << endl;
-    Type* sheep = new Type{Type::Sheep};
-    gbMap->getSquare(1, 3)->getBottomLeft()->setType(sheep);
-    hand->exchange();
-    cout << "Actual sheep resources gained at (1, 3): " << *hand->getSheepResourceMarker();
 }
